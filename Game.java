bc6ad2518c5675029900bb022bb812437a85be43
JoSuh <52906059+JoSuh@users.noreply.github.com>
2019-09-08 12:40:47 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,24 @@
+/****************************************************************************
+ *
+ * Created by: Heejo Suh
+ * Created on: Mar 2018
+ * Created for: learning
+ * 
+ * This program uses the stack 'Blackjack' to play the game
+ *
+ ****************************************************************************/
+
+
+//****************************************************************************
+public class Game {
+	//This class tests the stacks and play blackjack
+
+
+	//-----------------------
+	public static void main(String[] args) throws Exception {
+		//instantiate triangle given the input and calculate values
+		System.out.println("=================================================\nWelcome to Blackjack\n=================================================\n");
+
+		Blackjack newGame = new Blackjack();
+	}
+}
